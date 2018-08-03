3e44c0f6a54b61f31855cb28fa7c43931f517c6b
Julen Diaz Cosme <len.diazcosme@gmail.com>
2018-08-03 10:56:55 -0500

    Added file relating to condiontal statements using numbers

---
+++ conditional_tests_numbers.py
@@ -0,0 +1,27 @@
+# Conditional tests using numbers
+answer = 22
+if answer != 44:
+    print("That is not the correct answer. Please try again!")
+
+age = 23
+
+age < 45
+age <= 45
+age > 45
+age >= 45
+
+# Changing age_1 to 33 to make it pass "True"
+age_0 = 33
+age_1 = 14
+age_0 >= 31 and age_1 >= 31
+age_1 = 33
+age_0 >= 31 and age_1 >= 31
+
+# Using "or" operator
+age_0 = 33
+age_1 = 14
+age_0 >= 31 or age_1 >= 31
+age_0 = 14
+age_0 >= 31 or age_1 >= 31
+
+
