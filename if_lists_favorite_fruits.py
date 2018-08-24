bd71423acbc03d85a50dc19ad30b8e8f3481d8ba
Julen Diaz Cosme <len.diazcosme@gmail.com>
2018-08-24 13:30:05 -0500

    Added file relating to if statements and lists using favorite fruits

---
+++ if_lists_favorite_fruits.py
@@ -0,0 +1,21 @@
+# Favorite Fruit: Make a list of your favorite fruits, and then write a
+# series of independent if statements that check for certain fruits in your list.
+# 
+# Make a list of your three favorite fruits and call it favorite_fruits.
+#
+# Write five if statements.
+# Each should check whether a certain kind of fruit is in your list.
+# If the fruit is in your list, the if block should print a statement, 
+# such as You really like bananas!
+
+favorite_fruits= ['strawberries', 'pineapple', 'blueberries', 'watermelon']
+
+if 'strawberries' in favorite_fruits:
+    print("Strawberries in list.")
+if 'pineapple' in favorite_fruits:
+    print("Pineapple in list.")
+if 'blueberries' in favorite_fruits:
+    print("Blueberries in list.")
+if 'watermelon' in favorite_fruits:
+    print("Watermelon in list.")
+
