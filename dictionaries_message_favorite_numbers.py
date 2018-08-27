864fad2912d26ec057862f7e2f826874874b49e2
Julen Diaz Cosme <len.diazcosme@gmail.com>
2018-08-27 13:40:27 -0500

    Added file relating to dictionaries and messages using favorite numbers

---
+++ dictionaries_message_favorite_numbers.py
@@ -0,0 +1,21 @@
+# Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+#
+# Think of five names, and use them as keys in your dictionary.
+# Think of a favorite number for each person, and store each as a value in
+# your dictionary.
+#
+# Print each person’s name and their favorite number.
+
+favorite_numbers = {
+  'J': '6',
+  'Jo': '4',
+  'Fin': '7',
+  'Juniper': '3',
+  'Li': '0',
+  }
+
+print("J's favorite number is " + favorite_numbers['J'] + ".")
+print("Jo's favorite number is " + favorite_numbers['Jo'] + ".")
+print("Fin's favorite number is " + favorite_numbers['Fin'] + ".")
+print("Juniper's favorite number is " + favorite_numbers['Juniper'] + ".")
+print("Li's favorite number is " + favorite_numbers['Li'] + ".")
