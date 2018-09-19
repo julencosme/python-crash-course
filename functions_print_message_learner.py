e20ee6c74fc762de6ca6ac3de310debeda69165b
Julen Diaz Cosme <len.diazcosme@gmail.com>
2018-09-19 13:22:06 -0500

    Added file relating to functions and printing a simple message using a learner and the course objective

---
+++ functions_print_message_learner.py
@@ -0,0 +1,12 @@
+# Message. Write a function called 'display_message()' that prints one
+# sentence telling everyone what you are learning about in this chapter.
+#
+# Call the function, and make sure the message displays correctly.
+
+
+def display_message(learner):
+    """Display a simple message concerning the chapeter on functions."""
+    print("Hello, " + learner + ": in this chapter we are covering functions!")
+
+
+display_message('Teresa')
