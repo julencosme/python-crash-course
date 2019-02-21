c5a6f8581593baf22abb72ce63748719b428475c
Julen Diaz Cosme <len.diazcosme@gmail.com>
2019-02-21 14:55:54 -0600

    Added two files concerning magician module

---
+++ imports_magicians.py
@@ -0,0 +1,13 @@
+# Importing methods conerning magician names:
+# 'make_great' and 'show_magicians' from, 'magicians.py' module.
+
+
+from magicians import make_great, show_magicians
+
+# example data:
+original_magician_names = ['rockwell', 'wellrock', 'lockrow']
+modified_magician_names = []
+
+
+make_great(original_magician_names, modified_magician_names)
+show_magicians(modified_magician_names)
---
+++ magicians.py
@@ -0,0 +1,17 @@
+# Module for methods pertaining to Magician Names
+
+
+def make_great(original_magician_names, modified_magician_names):
+    """Modifying an existing list of magician names to read "the Great" along with
+    their names."""
+    while original_magician_names:
+        current_name = original_magician_names.pop()
+        print("The Great " + current_name.title())
+        modified_magician_names.append(current_name)
+
+
+def show_magicians(modified_magician_names):
+    """Show the list of modified magician names."""
+    print("\nThe following magician names have been modified:")
+    for modified_magician_name in modified_magician_names:
+        print(modified_magician_name)
