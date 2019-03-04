9b1b1409ce8a2490d214b752c435e0e4bc3ff9fd
Julen Diaz Cosme <len.diazcosme@gmail.com>
2019-03-04 14:49:01 -0600

    Added two files relating to importing functions and magicians

---
+++ imports_functions_modify_list_magicans_ii.py
@@ -0,0 +1,9 @@
+# Importing functions from 'imports_printing_functions_magicians.py'.
+
+from imports_printing_functions_magicians import make_great, show_magicians
+
+original_magician_names = ['rockwell', 'wellrock', 'lockrow']
+modified_magician_names = []
+
+make_great(original_magician_names, modified_magician_names)
+show_magicians(modified_magician_names)
